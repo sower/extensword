cfb69ba1d3a695cb5207311d286c63bee5406143
XLZY <1017866168@qq.com>
2022-12-29 12:41:54 +0800

    fix: content_scripts don't work in dev (#113)
    * chore: remove useless comment
    
    * fix: content_scripts don't work

--- src/manifest.ts
+++ src/manifest.ts
@@ -55,13 +55,8 @@ export async function getManifest() {
     },
   }
 
-  if (isDev) {
-    // for content script, as browsers will cache them for each reload,
-    // we use a background script to always inject the latest version
-    // see src/background/contentScriptHMR.ts
-    delete manifest.content_scripts
+  if (isDev)
     manifest.permissions?.push('webNavigation')
-  }
 
   return manifest
 }
